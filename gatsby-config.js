e45a9e16cf32323ce1f14d4afec95faf548eb874
robhsc <rob.humar@socialchain.com>
2019-07-10 15:20:46 +0100

    Added gatsby netlify cache

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,6 +54,9 @@ module.exports = {
       resolve: `gatsby-source-contentful`,
       options: contentfulConfig,
     },
+    {
+      resolve: "gatsby-plugin-netlify-cache"
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
