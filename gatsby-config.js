27e1dbc2f7625efbb4bdc688515d1146f715e272
robhsc <rob.humar@socialchain.com>
2019-07-10 16:10:44 +0100

    Updated Gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,15 +28,6 @@ module.exports = {
     author: `@humet`,
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
