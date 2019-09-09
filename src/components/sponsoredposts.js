01193b66712d6d82e7b130ac2f64b99bdcbc3047
robhsc <rob.humar@socialchain.com>
2019-09-09 14:52:30 +0100

    Added revcontent

--- src/components/sponsoredposts.js
+++ src/components/sponsoredposts.js
@@ -1,6 +1,9 @@
 
 import React from 'react'
 import ThemeStyles from '../styles/theme.js'
+import './sponsoredposts.scss'
+
+let data
 
 class SponsoredPosts extends React.Component {
   constructor(props) {
@@ -8,7 +11,6 @@ class SponsoredPosts extends React.Component {
     this.state = {
       error: null,
       isLoaded: false,
-      content: ''
     };
   }
 
@@ -17,10 +19,9 @@ class SponsoredPosts extends React.Component {
     .then(res => res.json())
     .then(
       (result) => {
-        console.log(result)
+        data = result.content
         this.setState({
-          isLoaded: true,
-          content: result.content
+          isLoaded: true
         });
       },
       (error) => {
@@ -32,6 +33,33 @@ class SponsoredPosts extends React.Component {
     )
   }
 
+  showRevcontent(data) {
+    const items = data.content.map(item => (
+        <div key={item.uid} style={{ position: `relative`, padding: `0 15px`}} className="articlepreview sponsoredposts--item">
+        <a href={item.url} target="_blank" rel="noopener noreferrer">
+         <img src={item.image} alt={item.headline} width="100%" />
+        </a>
+        <div style={{ position: `relative`, top: `-40px`, width: '100%' }}>
+          <div style={{ background: `#fff`, padding: `15px`, width: `80%`, margin: `0 auto`, textAlign: `center`, border: ThemeStyles.borders.primary }}>
+          <div className="small" style={{ textTransform: `uppercase` }}>
+            <a href={item.url} target="_blank" rel="noopener noreferrer" style={{
+                color: ThemeStyles.colour.primary,
+                fontWeight: `bold`,
+                textDecoration: `none`,
+                }}>{ item.brand }</a>
+        </div>
+            <h3 style={{marginBottom: `5px`}}>
+              <a href={item.url} target="_blank" rel="noopener noreferrer" style={{ color: ThemeStyles.colour.body, textDecoration: `none`}}>{item.headline}</a>
+            </h3>
+  
+          </div>
+        </div>
+      </div>
+    ))
+
+    return items
+  }
+
   render() {
     const { error, isLoaded, content } = this.state;
     let status
@@ -41,12 +69,14 @@ class SponsoredPosts extends React.Component {
     } else if (!isLoaded) {
       status = <div>Loading...</div>
     } else {
-      status = <p>LOADED</p>
+      status = this.showRevcontent(data)
     }
     return(
     <section style={{background: ThemeStyles.colour.primary}}>
       <h2 style={{ textTransform: `uppercase`, color: `#fff`, borderBottom: `#fff solid 3px`, paddingBottom: `15px`}}>Sponsored Content</h2>
+      <div className="sponsoredposts" style={{margin: `0 -15px`}}>
       {status}
+      </div>
     </section>
     )
   }
